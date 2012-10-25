0b16112fc8344bbbc0d78c129acd4329a1932e0b
spock <spock@cosmo.ecn.purdue.edu>
2012-10-24 21:17:53 -0400

    trying to determine per cpu offset

--- tstruct.c
+++ tstruct.c
@@ -20,12 +20,31 @@ static inline long long int read_gs(void)
     return i;
 }
 
+static inline struct task_struct *try_current_task(void)
+{
+    unsigned long long cur_task_maybe;
+    unsigned int offset;
+    offset = 0xb000;
+    asm("\t movq %%gs:%P1, %0" : "=r"(cur_task_maybe) : "m"(offset));
+    //cur_task_maybe = (struct task_struct *)(native_read_msr(MSR_GS_BASE) + 0xb000);
+    printk (KERN_INFO "value is %llu\n", cur_task_maybe);
+    return (struct task_struct *) cur_task_maybe;
+}
+
+
 static int __init tstruct_init(void)
 {
     struct task_struct *cur_task;
+    struct task_struct *cur_task_maybe;
     int id;
     long long int gs_val;
     cur_task = get_current();
+    cur_task_maybe = try_current_task();
+    if (cur_task == cur_task_maybe) {
+	printk (KERN_INFO "success");
+    } else {
+	printk (KERN_INFO "failure");
+    }
     printk(KERN_INFO "offsets: ts_state=%ld, ts_pid=%ld, ts_next=%ld ts_comm=%ld, ts_prev=%ld\n", 
             (long) offsetof(struct task_struct, state),
             (long) offsetof(struct task_struct, pid),
@@ -34,7 +53,8 @@ static int __init tstruct_init(void)
             (long) offsetof(struct task_struct, tasks.prev));
     
     printk(KERN_INFO "task is %p\n", cur_task);
-    
+    printk(KERN_INFO "task may be %p\n", cur_task_maybe);    
+
     id = __my_cpu_offset;
     printk(KERN_INFO "cpu offset is %d\n", id);
     //printk(KERN_INFO "offset is %p\n", per_cpu_var(current_task));
